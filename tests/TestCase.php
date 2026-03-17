fda6fc32dc82605a07db5eb9c93934be8ef9cd25
maxwellmezadre <maxwellmezadre@gmail.com>
2026-03-17 13:38:54 -0300

    fix: disable vite during tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,15 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Override;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    #[Override]
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
